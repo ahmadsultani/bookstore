7b56833426558c97dc32c2cbca9860ce0c6f4dc2
Andiilul <hafidamfa21d@student.unhas.ac.id>
2023-03-31 14:06:21 +0800

    updt

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <nav className="">
+    <nav>
         <div>
           <p>BookStore</p>
         </div>
-        <ul className="">
+        <ul>
             <li className= "flex w-[180px] text-[15px] font-normal justify-center align-middle h-[80px] bg-white" >Home</li>
             <li className=''>Shop</li>
-            <li className="">Categories</li>
-            <li className="">About Us</li>
+            <li>Categories</li>
+            <li>About Us</li>
         </ul>
         <div className="flex items-center justify-center gap-[20px] bg-black">
-          <button className="">Sign In</button>
-          <button className="">Sign Up</button>
+          <button>Sign In</button>
+          <button>Sign Up</button>
         </div>
 
     </nav>

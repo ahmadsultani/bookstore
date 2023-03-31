8688102f3d9b59a3fd0ce7c670eba8d29e7ec5c5
Andiilul <hafidamfa21d@student.unhas.ac.id>
2023-03-31 13:40:11 +0800

    update 13.40

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,20 @@
 import React from 'react'
-import styles from '../styles/Navbar.module.css'
 
 function Navbar() {
   return (
-    <nav className={styles.NavContainer}>
+    <nav className="">
         <div>
           <p>BookStore</p>
         </div>
-        <ul className={styles.NavItemsCtr}>
-            <li className={styles.NavItems}>Home</li>
-            <li className={styles.NavItems}>Shop</li>
-            <li className={styles.NavItems}>Categories</li>
-            <li className={styles.NavItems}>About Us</li>
+        <ul className="">
+            <li className= "flex w-[180px] text-[15px] font-normal justify-center align-middle h-[80px] bg-white" >Home</li>
+            <li className=''>Shop</li>
+            <li className="">Categories</li>
+            <li className="">About Us</li>
         </ul>
-        <div className="flex items-center justify-center gap-[20px]">
-          <button className={styles.signin}>Sign In</button>
-          <button className={styles.signup}>Sign Up</button>
+        <div className="flex items-center justify-center gap-[20px] bg-black">
+          <button className="">Sign In</button>
+          <button className="">Sign Up</button>
         </div>
 
     </nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,6 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <Navbar/>
-        Hello World
       </main>
     </>
   )

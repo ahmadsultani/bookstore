358ddcf9eab0fb7d207205338e6337b2718d71a4
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-03-31 14:00:11 +0800

    fix error tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,9 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <Navbar/>
+        <p className='flex bg-red '>
+          adsa
+        </p>
       </main>
     </>
   )

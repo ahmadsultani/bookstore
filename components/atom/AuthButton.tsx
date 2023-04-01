d6ca2330cb3abbf259aa29a1951c70172200b24a
Andiilul <hafidamfa21d@student.unhas.ac.id>
2023-04-01 16:52:54 +0800

    add hover in AuthButton.tsx

--- components/atom/AuthButton.tsx
+++ components/atom/AuthButton.tsx
@@ -9,7 +9,7 @@ export default function (props: InputProps) {
   const {fill} = props;
   return (
     <>
-      <button className='flex bg-green text-darkgreen rounded-[100px] px-[40px] text-[20px] py-[5px]'>{fill}</button>
+      <button className='flex bg-green text-darkgreen border-[1px] border-green hover:bg-transparent hover:text-green hover:duration-[100ms] duration-[100ms] rounded-[100px] px-[40px] text-[20px] py-[5px] hover'>{fill}</button>
 
     </>
   )

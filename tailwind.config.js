8d8bfad298a357470d120c41b2b04916460940e5
Andiilul <hafidamfa21d@student.unhas.ac.id>
2023-04-01 16:14:23 +0800

    edit configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#001e2b",
+        green:"#00ed64",
+        darkgreen:"#00684a",
 
+			},
+		},
+		plugins: [],
+	},
+};

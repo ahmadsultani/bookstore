4a8cf4f276462fd9d79dd73be36e3b52df693ff7
Andiilul <hafidamfa21d@student.unhas.ac.id>
2023-04-04 08:24:42 +0800

    make changes to login and signup

--- pages/login.tsx
+++ pages/login.tsx
@@ -35,25 +35,25 @@ export default function login() {
 						</div>
 					</div>
 					<div className="flex flex-col grow py-10 px-[80px] justify-center bg-white gap-[50px]">
-						<h1 className="text-darkgreen font-semibold text-[40px] text-center">
+						<h1 className="text-darkgreen font-bold text-[40px] text-center">
 							Sign In
 						</h1>
 						<div className="flex flex-col gap-[20px]">
-							<Input label="" placeholder="Username" icon="user.svg" />
+							<Input label="" placeholder="Email" icon="user.svg" />
 							<Input label="" placeholder="Password" icon="locks.svg" />
 							<div className="flex justify-between text-darkgreen text-[12px] ">
 								<div className="flex gap-[5px]">
 									<input type="checkbox"></input>
 									<p>Remember Me</p>
 								</div>
-								<p>Forgot Password?</p>
+								<Link className="hover:font-semibold hover:underline underline-offset-2" href={'/'}>Forgot Password?</Link>
 							</div>
 						</div>
 						<div className="flex justify-center">
 							<AuthButton fill={'Sign In'}/>
 						</div>
 						<div className="flex self-center gap-1 text-[15px]">
-							<p>New to Bookstore?</p><Link href={'signup'} className="text-green font-bold hover:underline ">Create Account</Link>
+							<p>New to Bookstore?</p><Link href={'signup'} className="text-darkgreen font-bold hover:underline underline-offset-2">Create Account</Link>
 						</div>
 					</div>
 				</div>

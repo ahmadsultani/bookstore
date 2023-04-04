8d6370b41ac165c8b6c38f20279b4172b0fc6d1f
Andiilul <hafidamfa21d@student.unhas.ac.id>
2023-04-04 08:26:48 +0800

    make change auth button (hover)

--- components/atom/AuthButton.tsx
+++ components/atom/AuthButton.tsx
@@ -1,16 +1,14 @@
-import React from 'react'
+import React from "react";
 
 interface InputProps {
 	fill: string;
-
 }
 
 export default function (props: InputProps) {
-  const {fill} = props;
-  return (
-    <>
-      <button className='flex bg-green text-darkgreen border-[1px] border-green hover:bg-transparent hover:text-green hover:duration-[100ms] duration-[100ms] rounded-[100px] px-[40px] text-[20px] py-[5px] hover'>{fill}</button>
-
-    </>
-  )
+	const { fill } = props;
+	return (
+		<button className="flex grow justify-center font-semibold bg-green text-primary border-[1px] hover:bg-[#06b850] transition-all duration-200 ease-in-out rounded-[100px] text-[18px] py-[8px] hover">
+			{fill}
+		</button>
+	);
 }

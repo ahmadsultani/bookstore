801fdb24dd30c0a31a29f786a03822d9108083cf
Andiilul <hafidamfa21d@student.unhas.ac.id>
2023-04-07 11:39:24 +0800

    update navbar

--- components/organisms/Navbar.tsx
+++ components/organisms/Navbar.tsx
@@ -1,43 +1,65 @@
 import Link from "next/link";
 import { useState, useEffect } from "react";
 
-function Navbar() {
+function Navbar({ onLanding }: { onLanding?: boolean }) {
 	const [scrollPosition, setScrollPosition] = useState(0);
+	useEffect(() => {
+		const handleScroll = () => {
+			const position = window.pageYOffset;
+			setScrollPosition(position);
+		};
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const position = window.pageYOffset;
-      setScrollPosition(position);
-    };
+		window.addEventListener("scroll", handleScroll, { passive: true });
 
-    window.addEventListener("scroll", handleScroll, { passive: true });
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, [scrollPosition]);
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [scrollPosition]);
+	const bgclass =
+		scrollPosition > 720 && "bg-gradient-to-b from-black to-[rgb(0,0,0,0.3)] ";
+	const buttonhover = scrollPosition > 720 && "hover:text-green";
 
-  const bgclass = scrollPosition > 720 ? " bg-primary shadow-[0_25px_60px_-15px_rgba(0,0,0,0.6)]" : " bg-gradient-to-b from-black to-[rgb(0,0,0,0.3)] ";
-	const buttonhover = scrollPosition >720? "hover:bg-darkgreen":"hover:text-green";
+	const navitems = [
+		{
+			title: "Home",
+			href: "/"
+		},
+		{
+			title: "Shop",
+			href: "/shop"
+		},
+		{
+			title: "Category",
+			href: "/category"
+		},
+		{
+			title: "About us",
+			href: "/about-us"
+		},
+	]
 
 	return (
-		<nav className={`duration-[400ms] z-50 fixed text-white flex w-[100%] top-0 max-w-[100vw] ${bgclass} px-[100px] py-[15px]`}> 
+		<nav
+			className={` duration-[400ms] z-50 ${onLanding ? "fixed": "sticky"} text-white flex w-[100%] top-0 max-w-[100vw] bg-primary shadow-[0_25px_60px_-15px_rgba(0,0,0,0.6)] ${
+				onLanding && bgclass
+			} px-[100px] py-[15px]`}
+		>
 			<div className="flex grow items-center px-[20px] font-bold text-white text-[24px]">
 				<p>BookStore</p>
 			</div>
 			<ul className="flex gap-[10px]">
-				<li className={`flex w-[120px] py-[12px] font-normal justify-center align-middle rounded-[8px] ${buttonhover}  items-center duration-[200ms] hover:duration-[200ms] `}>
-					Home
-				</li>
-				<li className={`flex w-[120px] py-[12px] font-normal justify-center align-middle rounded-[8px] ${buttonhover}  items-center duration-[200ms] hover:duration-[200ms] `}>
-					Shop
-				</li>
-				<li className={`flex w-[120px] py-[12px] font-normal justify-center align-middle rounded-[8px] ${buttonhover}  items-center duration-[200ms] hover:duration-[200ms] `}>
-					Categories
-				</li>
-				<li className={`flex w-[120px] py-[12px] font-normal justify-center align-middle rounded-[8px] ${buttonhover}  items-center duration-[200ms] hover:duration-[200ms] `}>
-					About Us
-				</li>
+				{navitems.map((navitem, index) => (
+					<Link
+						key={index}
+						href={navitem.href}
+						className={`flex w-[120px] py-[12px] font-normal justify-center align-middle rounded-[8px]  hover:bg-darkgreen ${
+							onLanding && buttonhover
+						}  items-center duration-[200ms] hover:duration-[200ms] `}
+					>
+						{navitem.title}
+					</Link>
+				))}
 			</ul>
 			<div className="flex items-center justify-center px-[20px] gap-[20px]">
 				<Link

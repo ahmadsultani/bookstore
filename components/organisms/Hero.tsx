cb171cc948d59189e7cae938bde78855274d1d6e
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-04-07 11:56:49 +0800

    fix hero and navbar

--- components/organisms/Hero.tsx
+++ components/organisms/Hero.tsx
@@ -3,33 +3,31 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function Hero() {
-	return (
-		<main className="flex items-center justify-center bg-darkgreen max-h-screen overflow-hidden ">
-			<div className="h-[100%] w-[100%] ">
-				<Image
-					src={"/assets/images/Library.webp"}
-					alt=""
-					width={4000}
-					height={4000}
-					className="object-cover opacity-70"
-				/>
-			</div>
-			<div className="flex font-bold text-white absolute h-[100%] w-[100%] px-[25px] pb-[25px]  pt-[100px]">
-				<div className="flex flex-col gap-[40px] h-[100%] w-[100%] items-center justify-center ">
-					<section className="flex flex-col items-center">
-						<h1 className="font-bold text-[80px]">Where Books Are Treasured</h1>
-						<h2 className="text-[30px] text-[rgba(255,255,255,0.8)]">
-							The start of a lifelong adventure
-						</h2>
-					</section>
-					<Link
-						href={"signup"}
-						className="flex text-[20px] items-center px-[40px] py-[16px] justify-center rounded-[8px] text-primary border-[1px] bg-green hover:bg-transparent  hover:border-green hover:text-green border-green duration-[200ms] hover:duration-[200ms]"
-					>
-						Get Started
-					</Link>
-				</div>
-			</div>
-		</main>
-	);
+  return (
+    <main className="flex items-center justify-center bg-darkgreen h-screen overflow-hidden ">
+      <Image
+        src={"/assets/images/Library.webp"}
+        alt=""
+        width={4000}
+        height={4000}
+        className="object-cover absolute opacity-70 z-10 h-screen"
+      />
+      <div className="flex font-bold text-white px-[25px] z-20 pt-[50px]">
+        <div className="flex flex-col gap-[40px] h-[100%] w-[100%] items-center justify-center ">
+          <section className="flex flex-col items-center">
+            <h1 className="font-bold text-[80px]">Where Books Are Treasured</h1>
+            <h2 className="text-[30px] text-[rgba(255,255,255,0.8)]">
+              The start of a lifelong adventure
+            </h2>
+          </section>
+          <Link
+            href={"signup"}
+            className="flex text-[20px] items-center px-[40px] py-[16px] justify-center rounded-[8px] text-primary border-[1px] bg-green hover:bg-transparent  hover:border-green hover:text-green border-green duration-[200ms] hover:duration-[200ms]"
+          >
+            Get Started
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 }
--- components/organisms/Navbar.tsx
+++ components/organisms/Navbar.tsx
@@ -2,81 +2,82 @@ import Link from "next/link";
 import { useState, useEffect } from "react";
 
 function Navbar({ onLanding }: { onLanding?: boolean }) {
-	const [scrollPosition, setScrollPosition] = useState(0);
-	useEffect(() => {
-		const handleScroll = () => {
-			const position = window.pageYOffset;
-			setScrollPosition(position);
-		};
+  const [scrollPosition, setScrollPosition] = useState(0);
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.pageYOffset;
+      setScrollPosition(position);
+    };
 
-		window.addEventListener("scroll", handleScroll, { passive: true });
+    window.addEventListener("scroll", handleScroll, { passive: true });
 
-		return () => {
-			window.removeEventListener("scroll", handleScroll);
-		};
-	}, [scrollPosition]);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrollPosition]);
 
-	const bgclass =
-		scrollPosition > 720 && "bg-gradient-to-b from-black to-[rgb(0,0,0,0.3)] ";
-	const buttonhover = scrollPosition > 720 && "hover:text-green";
+  const bgclass =
+    scrollPosition < 720 &&
+    "bg-transparent bg-gradient-to-b from-black to-[rgb(0,0,0,0.3)] ";
+  const buttonhover = scrollPosition < 720 && "hover:text-green hover:bg-transparent";
 
-	const navitems = [
-		{
-			title: "Home",
-			href: "/"
-		},
-		{
-			title: "Shop",
-			href: "/shop"
-		},
-		{
-			title: "Category",
-			href: "/category"
-		},
-		{
-			title: "About us",
-			href: "/about-us"
-		},
-	]
+  const navitems = [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "Shop",
+      href: "/shop",
+    },
+    {
+      title: "Category",
+      href: "/category",
+    },
+    {
+      title: "About us",
+      href: "/about-us",
+    },
+  ];
 
-	return (
-		<nav
-			className={` duration-[400ms] z-50 ${onLanding ? "fixed": "sticky"} text-white flex w-[100%] top-0 max-w-[100vw] bg-primary shadow-[0_25px_60px_-15px_rgba(0,0,0,0.6)] ${
-				onLanding && bgclass
-			} px-[100px] py-[15px]`}
-		>
-			<div className="flex grow items-center px-[20px] font-bold text-white text-[24px]">
-				<p>BookStore</p>
-			</div>
-			<ul className="flex gap-[10px]">
-				{navitems.map((navitem, index) => (
-					<Link
-						key={index}
-						href={navitem.href}
-						className={`flex w-[120px] py-[12px] font-normal justify-center align-middle rounded-[8px]  hover:bg-darkgreen ${
-							onLanding && buttonhover
-						}  items-center duration-[200ms] hover:duration-[200ms] `}
-					>
-						{navitem.title}
-					</Link>
-				))}
-			</ul>
-			<div className="flex items-center justify-center px-[20px] gap-[20px]">
-				<Link
-					href={"login"}
-					className={`flex w-[120px] py-[12px] font-normal justify-center align-middle rounded-[8px] ${buttonhover}  items-center duration-[200ms] hover:duration-[200ms] `}
-				>
-					Sign In
-				</Link>
-				<Link
-					href={"signup"}
-					className="flex items-center py-[12px] justify-center w-[100px] rounded-[8px] text-primary border-[1px] bg-green hover:bg-transparent  hover:border-green hover:text-green border-green duration-[200ms] hover:duration-[200ms]"
-				>
-					Sign Up
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav
+      className={` duration-[400ms] z-50 bg-primary ${
+        onLanding ? "fixed " + bgclass : "sticky"
+      } text-white flex w-[100%] top-0 max-w-[100vw] shadow-[0_25px_60px_-15px_rgba(0,0,0,0.6)] px-[100px] py-[15px] `}
+    >
+      <div className="flex grow items-center px-[20px] font-bold text-white text-[24px]">
+        <p>BookStore</p>
+      </div>
+      <ul className="flex gap-[10px]">
+        {navitems.map((navitem, index) => (
+          <Link
+            key={index}
+            href={navitem.href}
+            className={`flex w-[120px] py-[12px] font-normal justify-center align-middle rounded-[8px] hover:bg-darkgreen ${
+              onLanding && buttonhover
+            }  items-center duration-[200ms] hover:duration-[200ms] `}
+          >
+            {navitem.title}
+          </Link>
+        ))}
+      </ul>
+      <div className="flex items-center justify-center px-[20px] gap-[20px]">
+        <Link
+          href={"login"}
+          className={`flex w-[120px] py-[12px] font-normal justify-center align-middle rounded-[8px]  hover:bg-darkgreen ${buttonhover}  items-center duration-[200ms] hover:duration-[200ms] `}
+        >
+          Sign In
+        </Link>
+        <Link
+          href={"signup"}
+          className="flex items-center py-[12px] justify-center w-[100px] rounded-[8px] text-primary border-[1px] bg-green hover:bg-transparent  hover:border-green hover:text-green border-green duration-[200ms] hover:duration-[200ms]"
+        >
+          Sign Up
+        </Link>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;

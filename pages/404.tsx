951f5c41133d6beeff3c7595b9c2deae1f637a9a
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-04-08 18:41:37 +0800

    fix:top value

--- pages/404.tsx
+++ pages/404.tsx
@@ -6,7 +6,7 @@ export default function Custom404() {
   return (
     <main className="bg-gradient-to-r from-darkgreen to-primary  w-full h-screen ">
       <Navbar onLanding={true} />
-      <section className="absolute flex flex-col px-20 w-1/2 h-2/3 top-1/2 left-1/2 -translate-x-1/2 -translate-y-[40%] bg-white">
+      <section className="absolute flex flex-col px-20 w-1/2 h-2/3 top-1/2 left-1/2 -translate-x-1/2 -translate-y-[43%] bg-white">
         <h1 className="text-[160px] font-black bg-clip-text text-transparent bg-gradient-to-r from-darkgreen to-darkgreen text-center">
           404
         </h1>
